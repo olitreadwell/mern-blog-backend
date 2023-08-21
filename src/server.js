33a23b110cddfebf896eb3ce6c177852a8e1d8a6
olitreadwell <oliver.treadwell@gmail.com>
2023-08-20 20:05:10 -0700

    feat: include some more basic express routes

--- src/server.js
+++ src/server.js
@@ -1,10 +1,17 @@
 import express from 'express';
 
-const app = express();
 
-app.get('/hello', (req, res) => { 
-    res.send('Hello!'); 
+const app = express();
+app.use(express.json());
+
+app.post('/hello', (req, res) => { 
+    res.send(`POST Hello! ${req.body.name}`); 
 });
 
-app.listen(8888, () => console.log('Server is listening on port 8888...'));
+app.get('/hello/:name', (req, res) => {
+    const { name } = req.params;
+    res.send(`GET Hello! ${name}`);
+});
+
+app.listen(8888, () => console.log('Server is listening on port http://localhost:8888...'));
 
