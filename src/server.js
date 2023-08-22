c0fe41d0df49f4af939b88e7bd15e912f8674e4e
olitreadwell <oliver.treadwell@gmail.com>
2023-08-21 18:44:16 -0700

    refactor: use mongodb for upvotes endpoint

--- src/server.js
+++ src/server.js
@@ -6,46 +6,54 @@ app.use(express.json());
 
 // get specific article info endpoint
 app.get("/api/v1/articles/:name", async (req, res) => {
-    const { name } = req.params;
+  const { name } = req.params;
 
-    const client = await MongoClient.connect("mongodb://localhost:27017")   
+  const client = await MongoClient.connect("mongodb://localhost:27017");
 
-    const db = client.db("react-blog-db");
+  const db = client.db("react-blog-db");
 
-    const articleInfo = await db.collection("articles").findOne({ name });
+  const articleInfo = await db.collection("articles").findOne({ name });
 
-    if (articleInfo) {
-        res.status(200).json(articleInfo);
-    } else {
-        res.status(404).send("Article not found");
-    }
+  if (articleInfo) {
+    res.status(200).json(articleInfo);
+  } else {
+    res.status(404).send("Article not found");
+  }
 });
 
 // upvote article endpoint
-app.put("/api/v1/articles/:name/upvote", (req, res) => {
-    const { name } = req.params;
-    const article = articlesInfo.find((article) => article.name === name);
-    if (article) {
-        article.upvotes += 1;
-        res.status(200).send(`${name} now has ${article.upvotes} upvotes`);
-    } else {
-        res.status(404).send("Article not found");
-    }   
+app.put("/api/v1/articles/:name/upvote", async (req, res) => {
+  const { name } = req.params;
+
+  const client = await MongoClient.connect("mongodb://localhost:27017");
+
+  const db = client.db("react-blog-db");
+
+  await db.collection("articles").updateOne({ name }, { $inc: { upvotes: 1 } });
+
+  const updatedArticleInfo = await db.collection("articles").findOne({ name });
+
+  if (updatedArticleInfo) {
+    // article.upvotes += 1;
+    res.status(200).send(`${name} has ${updatedArticleInfo.upvotes} upvotes`);
+  } else {
+    res.status(404).send("Article not found");
+  }
 });
 
 // add comment endpoint
 app.post("/api/v1/articles/:name/comments/create", (req, res) => {
-    const { name } = req.params;
-    const { postedBy, text } = req.body;
+  const { name } = req.params;
+  const { postedBy, text } = req.body;
 
-    const article = articlesInfo.find((article) => article.name === name);
+  const article = articlesInfo.find((article) => article.name === name);
 
-    if (article) {
-        article.comments.push({ postedBy, text });
-        res.status(200).send(article.comments);
-    } else {
-        res.status(404).send("Article not found");
-    }
+  if (article) {
+    article.comments.push({ postedBy, text });
+    res.status(200).send(article.comments);
+  } else {
+    res.status(404).send("Article not found");
+  }
 });
 
 app.listen(8888, () =>
